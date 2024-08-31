ff4eab3c4175b756e07de0924dc61a5e6bf917b5
Alton Andrews <altonandrews@Altons-MacBook-Pro.local>
2024-08-30 22:36:08 -0500

    Greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import Greeting from './components/Greeting';
+
 function App() {
-  return <h1>Hello Atlas</h1>;
+  return (
+    <div>
+      <h1>Hello Atlas</h1>
+      <Greeting />
+    </div>
+  );
 }
 
 export default App;
